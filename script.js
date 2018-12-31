b645242c7e4fd37074c38fa66358bfe0ae672637
Keaton Hawkins <khawkins@ucvts.org>
2018-12-31 14:49:18 -0500

    copied code from repl.it

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function clickNum(number){
-  let p = document.getElementsByClassName("textview");
-  p.innerHTML += number;
+function insert(number){
+  if(number==99){
+    document.form.textview.value = 0;
+  }else if(document.form.textview.value=="0" && number==0){
+    document.form.textview.value=number;
+  }else if(document.form.textview.value=="0"){
+    document.form.textview.value = number;
+  }else if(document.form.textview.value!="0" && number==0){
+    document.form.textview.value += number;
+  }else{
+    document.form.textview.value += number;
+  }
+}
+function equal(){
+  let ans = document.form.textview.value;
+  if(ans){
+    document.form.textview.value = eval(ans);
+  }
 }
