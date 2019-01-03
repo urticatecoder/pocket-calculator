b236e888935da92a327a5d9b18cab65e87c529f2
Keaton Hawkins <githubuser12@gmail.com>
2019-01-03 10:51:48 -0500

    Fixed multiple operator issue

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 let x = 0;
+let z = 0;
 function insert(number){
+  //For clear button
   if(number==99){
     document.form.textview.value = 0;
-    x = 0;
+    x=0;
+    z=0;
+  //If you press 0 and what's in the display is just zero
   }else if(document.form.textview.value=="0" && number==0 && x==0){
     document.form.textview.value=number;
-    x = 0
+    x=0
+    z++;
+  //If you press a number, and what's in the display is just zero
   }else if(document.form.textview.value=="0" && x==0){
     document.form.textview.value = number;
-    x = 0;
+    x=0;
+    z++;
+  //If you try and enter more than one decimal point or operator in a row
+  }else if(isNaN(number)==true && x==0 && number==document.form.textview.value[document.form.textview.value.length-1]){
+    x=0;
+    z++;
+  //If you press 0 and what's in the display is not zero
   }else if(document.form.textview.value!="0" && number==0 && x==0){
     document.form.textview.value += number;
-    x = 0;
+    x=0;
+    z++;
+  //Regular case: what's in the display is not zero and you press a number or an operator
   }else if(x==0){
     document.form.textview.value += number;
-    x = 0
+    x=0;
+    z++;
+  //After you've pressed the equals sign, if you press a number
   }else if(x!=0 && number!="+" && number!="-" && number!="/" && number!="*"){
     document.form.textview.value = number;
     x=0;
+    z++;
+  //After you've pressed the equals sign, if you enter another operator
   }else if(x!=0 && number=="+" || x!=0 && number=="-" || x!=0 && number=="/" || x!=0 && number=="*"){
     document.form.textview.value += number;
     x=0;
+    z++;
+  }
+  let contents = document.form.textview.value;
+  if(z>3==0){
+    document.form.textview.value=document.form.textview.value
   }
 }
 function equal(){
